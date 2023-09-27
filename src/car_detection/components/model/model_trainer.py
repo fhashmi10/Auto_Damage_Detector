a2e4ea0fcbe86a86a7d54520ea81e5be9544e4c5
fhashmi10 <farazhashm@gmail.com>
2023-09-26 22:10:24 -0400

    update

--- src/car_detection/components/model/model_trainer.py
+++ src/car_detection/components/model/model_trainer.py
@@ -1,6 +1,7 @@
 """Module to train models"""
 import tensorflow as tf
-from src.car_detection.configuration.configuration_manager import DataConfig,ModelConfig,ParamConfig
+from src.car_detection.configuration.configuration_manager import \
+    DataConfig, ModelConfig, ParamConfig
 from src.utils.helper import build_dataset
 from src import logger
 
@@ -23,43 +24,57 @@ class ModelTrainer():
         except Exception as ex:
             raise ex
 
-    def train_model(self):
-        """Method to invoke model training"""
+    def get_dataset(self):
+        """Method to get dataset"""
         try:
             # Define image size
             image_size = tuple(map(int, self.params.image_size.split(',')))
-            if len(image_size)==3:
+            if len(image_size) == 3:
                 image_size = image_size[:2]
-            
+
             # Build training dataset
             train_ds, train_size = build_dataset(data_dir=self.data_config.data_path,
-                                     val_split=0.2,
-                                     subset="training",
-                                     image_size=image_size,
-                                     batch_size=self.params.batch_size)
-            
+                                                 val_split=0.2,
+                                                 subset="training",
+                                                 image_size=image_size,
+                                                 batch_size=self.params.batch_size)
+
             # Build validation dataset
             val_ds, val_size = build_dataset(data_dir=self.data_config.data_path,
-                                     val_split=0.2,
-                                     subset="validation",
-                                     image_size=image_size,
-                                     batch_size=self.params.batch_size)
+                                             val_split=0.2,
+                                             subset="validation",
+                                             image_size=image_size,
+                                             batch_size=self.params.batch_size)
             logger.info("Data loaded successfully.")
-            
+
             # Load transformer model
-            transformer_model = tf.saved_model.load(self.model_config.transform_model_path)
-            
+            transformer_model = tf.saved_model.load(
+                self.model_config.transform_model_path)
+
             # Apply transformer model on data
             train_ds = train_ds.map(lambda images, labels:
-                        (transformer_model(images), labels))
+                                    (transformer_model(images), labels))
             val_ds = val_ds.map(lambda images, labels:
                                 (transformer_model(images), labels))
             logger.info("Data transformed successfully.")
-            
+
+            return train_ds, train_size, val_ds, val_size
+        except AttributeError as ex:
+            logger.exception("Error getting dataset.")
+            raise ex
+        except Exception as ex:
+            raise ex
+
+    def train_model(self):
+        """Method to invoke model training"""
+        try:
+
+            # Get train and validate data
+            train_ds, train_size, val_ds, val_size = self.get_dataset()
+
             # Get built model to train
             model = self.get_built_model()
 
-            
             # Train the model
             steps_per_epoch = train_size // self.params.batch_size
             validation_steps = val_size // self.params.batch_size
@@ -68,9 +83,10 @@ class ModelTrainer():
                       epochs=self.params.number_epochs,
                       steps_per_epoch=steps_per_epoch,
                       validation_data=val_ds,
-                      validation_steps=validation_steps,
-                      verbose=2)
-            #model.save(self.config.trained_model_path)
+                      validation_steps=validation_steps)
+            
+            # Save trained model
+            model.save(self.model_config.trained_model_path)
         except AttributeError as ex:
             logger.exception("Error finding attribute: %s", ex)
             raise ex
